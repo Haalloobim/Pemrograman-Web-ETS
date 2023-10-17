58c20d5968d37af90b1268bad81c9bf46fbfc233
Haalloobim <withbimatara@gmail.com>
2023-10-17 08:06:51 +0700

    feat: creating 1 API card

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ let url = "http://159.223.51.203/api/bio";
 let card1 = (json) => {
     let name = json[0].name; 
     let id = json[0].id; 
-    
+    let name1 = document.getElementById("name1"); 
+    let id1 = document.getElementById("id1"); 
+    name1.innerHTML = name; 
+    id1.innerHTML = id; 
 }
 
 
@@ -14,6 +17,7 @@ let load = () => {
             let responseText = xhttp.responseText; 
             let json = JSON.parse(responseText); 
             console.log(json);
+            card1(json); 
         }
     }
     xhttp.open("GET", url, true);
